0e3bbb0ae0cde30e9d4b99ee0bce247d0f4e7bc3
Tony-M2 <155786343+Tony-M2@users.noreply.github.com>
2024-01-11 21:38:58 -0400

    Add files via upload

---
+++ Practica_1_origi.cpp
@@ -0,0 +1,35 @@
+//Titulo: Valores de tipos de datos
+//Autor: Luis Antonio Moreno Miranda 
+//Descripcion: Programa para probar los tipos de valores de datos
+//Fecha: 2024-01-11
+
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	short int numero_entero_p;
+	unsigned short int numero_entero_p1;
+	int numero_entero;
+	unsigned int numero_entero_s;
+	//float numero_decimal;
+	//double numero_decimal_g;
+	//asignamos valores limites de los tipos de datos
+	numero_entero_p=32767; //valor limite del tipo de dato
+	cout <<"Numero entero pequeño: " <<numero_entero_p<<endl;
+	numero_entero_p1=-6; //valor fuera del rango 65535 65530
+	cout <<"Numero entero pequeño positivo: " <<numero_entero_p1<<endl;
+	//asignar los valores maximos para unsigned int y int
+	numero_entero=2147483647;
+	cout <<"Numero entero " <<numero_entero<<endl;
+	/*numero_entero_s=2147483647;
+	cout <<"Numero entero " <<numero_entero_s<<endl;
+	//asignar los valores maximos para unsigned int y int
+	numero_entero_p1=numero_entero_p; //NO es correcta!. 
+	cout <<"Numero entero pequeño 1ra asignacion " <<numero_entero_p1<<endl;*/
+	
+	
+	
+	
+	return 0;
+}
